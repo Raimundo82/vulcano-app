e691e003a8785987a36bbf3b4c1e2eedbe58ddfc
Raimundo <pacheco.raimundo@marinha.pt>
2025-09-24 19:48:41 +0000

    chore: update postCreateCommand to include package installations in devcontainer setup

--- .devcontainer/post-script.sh
+++ .devcontainer/post-script.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-pip install --upgrade pip setuptools pre-commit
-pipx install poetry
-poetry env activate
-poetry install
-
 mysql --host=${DB_HOST} --user=${DB_USER} --password=${DB_PASSWORD} ${DB_NAME} < .devcontainer/vulcano_db.sql
