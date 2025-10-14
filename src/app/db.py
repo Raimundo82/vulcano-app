45c09383a71393f29dd9088c3ff73b0699da97f7
2TEN ST-EINF Pinheiro Domingues <pinheiro.domingues@marinha.pt>
2025-10-14 09:13:08 +0000

    Refactor app initialization into factory pattern and added `SQLAlchemy extension base` (#18)
    # App.py
    
    `src` > `app.py`- **simplificada**
    
    - `app.py` agora apenas cria e corre a Flask app via `create_app()`
    ```py
    from app import create_app
    app = create_app()
    ```
    
    # Adicionado factory logic em __init__.py
    
    - Introduzida função `create_app()`.
    - Configuração centralizada (`DevelopmentConfig` vs `ProductionConfig`).
    
    # Extensões extensions/db.py
    
    - Criado novo módulo para registo de extensões
    - Foram adicionadas estas seguintes instancias: `SQLAlchemy()` e `Migrate()`
    - Isto isola a inicialização da base de dados e prepara a migração de *queries cruas MySQL* para **SQLAlchemy ORM**
    
    <img width="206" alt="image.png" src="attachments/05d9f88f-f52f-4f47-8ec4-b4754e36100d">
    
    # DevContainer
    
    Continua a funcionar na mesma maneira, sem mudanças
    
    ```bash
    flask --app app.app run
    ```
    
    Reviewed-on: https://devops-01.marinha.pt/marinha-si/vulcano-app/pulls/18

--- src/app/db.py
+++ src/app/db.py
@@ -1,12 +1,19 @@
-# app/db.py
-import mysql.connector
-from app.config import Config
-
+from flask import current_app, g
+from app import mysql
 
 def get_connection():
-    return mysql.connector.connect(
-        host=Config.MYSQL_HOST,
-        user=Config.MYSQL_USER,
-        password=Config.MYSQL_PASSWORD,
-        database=Config.MYSQL_DB,
-    )
+    """
+    Returns connection from the Flask-MySQLdb extension
+    This connection is tied to the Flask app context.
+    """
+    if "db_conn" not in g:
+        g.db_conn = mysql.connection
+    return g.db_conn
+
+def close_connection():
+    """
+    Closes the database connection at the end of the request.
+    """
+    db_conn = g.pop("db_conn", None)
+    if db_conn is not None:
+        db_conn.close()
